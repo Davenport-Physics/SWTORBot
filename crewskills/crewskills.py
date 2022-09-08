05be5132aa0234d7d7e99f53fa6e9454da017cf2
Emma Davenport <davenport.physics@gmail.com>
2022-09-08 12:34:39 -0500

    Addition: set_config upon creation of CrewSkill object

--- crewskills/crewskills.py
+++ crewskills/crewskills.py
@@ -1,6 +1,7 @@
 import pyautogui
 from game_images import get_misc_image, get_skill_images
 from screenlib import ScreenImage
+from config import get_config
 
 def crew_skills_panel_opened():
 
@@ -36,7 +37,19 @@ class CrewSkill:
 
 		self.name   = name
 		self.coords = coords
+		self.set_config()
 
 	def __str__(self):
 
 		return "CrewSkill(name = {}, coords = {})".format(self.name, self.coords)
+
+	def set_config(self):
+
+		temp = get_config()
+		for crew_skill in temp["crew_skills"]:
+			if crew_skill["crew_skill_name"] == self.name:
+				self.config = crew_skill
+				return
+
+		raise "Unable to find appropriate config for {} crew_skill".format(self.name)
+
