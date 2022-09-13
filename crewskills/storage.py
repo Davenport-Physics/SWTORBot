515211951fafdd7c5b078c6b8b8888bd379650f1
Emma Davenport <davenport.physics@gmail.com>
2022-09-12 19:27:00 -0500

    Addition: save_mission_details

--- crewskills/storage.py
+++ crewskills/storage.py
@@ -1,8 +1,24 @@
 
 import sqlite3
 
+con = None
+cur = None
+
 def init_database():
+
+	global con
+	global cur
 
 	con = sqlite3.connect("crewskills.db")
 	cur = con.cursor()
-	cur.execute("CREATE TABLE IF NOT EXISTS AcceptedMission()")
+	with open("/sql/tables.sql", "r") as fp:
+		sql_commands = file.read()
+
+	cur.execute(text)
+	con.commit()
+
+def save_mission_details(mission):
+
+	sql_command = """INSERT INTO AcceptedMissions(MissionElapse, MissionCost) VALUES (?, ?)"""
+	cur.execute(sql_command, (mission.mission_time, mission.mission_cost))
+	con.commit()
